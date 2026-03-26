a1f45bbf8321bde6107fb146a8e24a3eb4b77cef
Ari G <ari.garner@education.nsw.gov.au>
2026-03-26 20:31:47 +0000

    Completed lines.py

---
+++ 6-FileIO/lines/lines.py
@@ -0,0 +1,22 @@
+import sys
+
+if len(sys.argv) == 2:
+    file = sys.argv[1]
+
+    linecount = 0
+
+    try:
+        file = open(file)
+    except:
+        sys.exit()
+
+    for line in file:
+        if line.lstrip().startswith("#") or line.lstrip() == "":
+            pass
+        else:
+            linecount += 1
+
+    print(linecount)
+
+else:
+    sys.exit()
