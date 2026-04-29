a29a7d70d823249c029ad7e89f10bd68d35149ca
Ari G <ari.garner@education.nsw.gov.au>
2026-04-29 23:34:17 +0000

    problem 3?

--- 5-UnitTests/test_plates/test_plates.py
+++ 5-UnitTests/test_plates/test_plates.py
@@ -2,28 +2,24 @@ import plates
 
 
 def test_in_char_range():
-    assert plates.is_valid("ABCD")
+    assert plates.is_valid("ABCD") == True
+
+
+def test_below_char_range():
+    assert plates.is_valid("A") == False
 
 
 def test_above_char_range():
     assert plates.is_valid("ABCDEFG") == False
 
 
-def test_below_characters():
-    assert plates.is_valid("A") == False
-
-
-def test_starts_with_numbers():
-    assert plates.is_valid("12CD") == False
-
-
 def test_non_alnum():
-    assert plates.is_valid("!@#$") == False
+    assert plates.is_valid("!BCD") == False
 
 
-def test_numbers_in_middle():
-    assert plates.is_valid("A23D") == False
+def test_first_num_0():
+    assert plates.is_valid("AB01") == False
 
 
-def test_first_number_is_0():
-    assert plates.is_valid("AB04") == False
+def test_alpha_after_num():
+    assert plates.is_valid("A12D") == False
